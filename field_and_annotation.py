ab52d3d7d237331f50d037191dc60115413cd9b6
rkydx <ramakant.c@gmail.com>
2026-01-17 14:38:33 +0800

    added column linkedinurl

--- field_and_annotation.py
+++ field_and_annotation.py
@@ -1,12 +1,13 @@
 # pip install pydantic
 # pip install email-validator
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, AnyUrl
 from typing import Annotated, List, Optional, Dict
 
 class Patient(BaseModel):
     name: Annotated[str, Field(max_length=50, title='Name of the Patient', description='Provide the name of the patient in less than 50 chars', examples=['Ramesh', 'Sam'])]
     email: EmailStr
+    linkedinUrl: AnyUrl
     age: int = Field(gt=0, lt=80)
     weight: Annotated[float, Field(gt=0, strict=True)]
     married: Annotated[bool, Field(default=None, description='is the patient married or not')]
@@ -21,8 +22,9 @@ def get_patient_data(patient: Patient):
     print("Patient's record has been fetched")
 
 patient_info = {'name': 'Ramesh',
-                'email': 'ramesh@gmail.com',            # email will be validated by using emailstr (must install email-validator for it to work)
-                'age': '40',                            # It will automatically converted to integer by pydantic
+                'email': 'ramesh@gmail.com',                    # email will be validated by using emailstr (must install email-validator for it to work)
+                'linkedinUrl': 'http://linkedin.com/abcd342d',  # Email validator
+                'age': '40',                                    # It will automatically converted to integer by pydantic
                 'weight': 76.3,
                 'contact_details': {
                     'phone': '4567890'
@@ -38,12 +40,12 @@ patient1 = Patient(**patient_info)
 This above line is exactly the same as:
 
 patient1 = Patient(
-    name='nitish',
-    email='abc@gmail.com',
-    linkedin_url='http://linkedin.com/1322',
-    age='30',
-    weight=75.2,
-    contact_details={'phone':'2353462'}
+    name='Ramesh',
+    email='ramesh@gmail.com',
+    linkedin_url='http://linkedin.com/abcd342d',
+    age='40',
+    weight=76.3,
+    contact_details={'phone':'4567890'}
 )
 
 '''
